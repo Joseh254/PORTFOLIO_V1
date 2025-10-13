6a15542cd5a053fc71baea53012c2ee6b2784e13
Joseh254 <josephkarimambugua@gmail.com>
2025-10-13 17:06:43 +0300

    add my images

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,4 +1,4 @@
-import person from "../../assets/images/person.png";
+import person from "../../assets/images/picture2.jpg";
 import "./introduction.css";
 import InformationSummary from "./InformationSummary";
 
